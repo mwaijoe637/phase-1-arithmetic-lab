890958c1010d64d54bf700854b944920c8826b44
joe <brakejoe9@gmail.com>
2024-03-26 07:51:14 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+console.log(multiply);
+
+
+const random =Math.floor(Math.random() * 10) + 1;
+
+console.log(random);
+
+
+
+const num3 = 32
+const num4 =7
+const mod = num3 % num4
+
+console.log(mod);
+
+
+
+
+const max = Math.max(1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,
+                      14,15,16,17,18,19,20);
+console.log(max)
+
